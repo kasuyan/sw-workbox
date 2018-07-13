8267a6dd80e3370f8cbcbcf60c3065209e236413
kasuya_kazunori <kasuya_kazunori@cyberagent.co.jp>
2018-07-13 18:56:40 +0900

    Updates

---
+++ js/precache-manifest.09ab147b2b865cc8a44f3645d0905c82.js
@@ -0,0 +1,6 @@
+self.__precacheManifest = [
+  {
+    "revision": "a30baa8e5638d5e1227b",
+    "url": "/js/main.js"
+  }
+];
---
+++ js/precache-manifest.97b3d8b58cc21b77a802e36757d5d162.js
@@ -0,0 +1,6 @@
+self.__precacheManifest = [
+  {
+    "revision": "e3f228a451a661733737",
+    "url": "/js/main.js"
+  }
+];
